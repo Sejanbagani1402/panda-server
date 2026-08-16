50da4ac3492b2c2703722e299bfa6f9ef829a920
Sejanbagani1402 <sejanbagani14@gmail.com>
2026-08-16 18:37:53 +0530

    Add Prisma v7 PostgreSQL driver adapter support

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,24 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(configService: ConfigService) {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
+    
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaPg({ connectionString: databaseUrl });
+    
+    super({
+      adapter,
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
